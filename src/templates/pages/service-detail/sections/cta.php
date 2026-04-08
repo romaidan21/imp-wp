d4f10a2294bc598999d5af8f489d3ea5e3f70179
Roman Maidan <romecom2210@gmail.com>
2026-04-08 12:15:21 +0300

    fix: remove gradient colors

--- src/templates/pages/service-detail/sections/cta.php
+++
@@ -1,5 +0,0 @@
-<div class="service-detail-page__cta">
-  <h3 class="service-detail-page__cta-title">Потрібна допомога у цій сфері?</h3>
-  <p class="service-detail-page__cta-description">Запишіться на первинну консультацію, щоб обговорити вашу ситуацію та дізнатися можливі шляхи вирішення.</p>
-  <a href="/contacts" class="service-detail-page__cta-button">Замовити консультацію</a>
-</div>
